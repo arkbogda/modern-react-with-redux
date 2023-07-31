26e5b480e795c7f78577edf5b5da7ec7e83ed9a0
ABogdan <Arkadiusz.Bogdan@external.t-mobile.pl>
2023-07-31 18:13:50 +0200

    styling with Bulma CSS Library

--- pdas/src/ProfileCard.js
+++ pdas/src/ProfileCard.js
@@ -1,4 +1,4 @@
-function ProfileCard({ title, handle, image }) {
+function ProfileCard({ title, handle, image, description }) {
 
     //1. taking entire Props Object
     // const title = props.title;
@@ -8,10 +8,20 @@ function ProfileCard({ title, handle, image }) {
     // const { title, handle } = props;
 
     return (
-        <div>
-            <img src={image} alt="pda logo"/>
-            <div>Title is {title}</div>
-            <div>Handle is {handle}</div>
+        <div className="card">
+            <div className="card-image">
+                <figure className="image is-1by1">
+                    <img src={image} alt="pda logo "/>
+                </figure>
+            </div>
+
+            <div className="card-content">
+                <div className="media-content">
+                    <p className="title is-4">{title}</p>
+                    <p className="subtitle is-6">  {handle}</p>
+                </div>
+                <div className="content"> {description} </div>
+            </div>
         </div>
     ); 
 }
